c2b6f1dc1f03327659519ee0677a533a92f9f6c3
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2010-07-20 11:24:58 +0200

    Store and restore link from/to in undo action

--- Cpg.Studio.Undo/Object.cs
+++ Cpg.Studio.Undo/Object.cs
@@ -6,20 +6,31 @@ namespace Cpg.Studio.Undo
 	{
 		private Wrappers.Group d_parent;
 		private Wrappers.Wrapper d_wrapped;
+		
+		private Wrappers.Wrapper d_from;
+		private Wrappers.Wrapper d_to;
 
 		public Object(Wrappers.Group parent, Wrappers.Wrapper wrapped)
 		{
 			d_parent = parent;
 			d_wrapped = wrapped;
+			
+			Wrappers.Link link = wrapped as Wrappers.Link;
+			
+			if (link != null)
+			{
+				d_from = link.From;
+				d_to = link.To;
+			}
 		}
 		
 		protected void DoAdd()
 		{
-			if (d_wrapped is Wrappers.Link)
+			Wrappers.Link link = d_wrapped as Wrappers.Link;
+			
+			if (link != null)
 			{
-				Wrappers.Link link = (Wrappers.Link)d_wrapped;
-				
-				link.Reattach();
+				link.Attach(d_from, d_to);
 			}
 
 			d_parent.Add(d_wrapped);
