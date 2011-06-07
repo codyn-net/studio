1d6e65db18f156d07549b622b5cb823eb3b865b0
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-06-07 09:50:18 +0200

    Unapply/reapply templates on delete/add

--- Cpg.Studio.Undo/Object.cs
+++ Cpg.Studio.Undo/Object.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Cpg.Studio.Undo
 {
@@ -9,11 +10,15 @@ namespace Cpg.Studio.Undo
 		
 		private Wrappers.Wrapper d_from;
 		private Wrappers.Wrapper d_to;
+		
+		private List<Wrappers.Wrapper> d_templates;
 
 		public Object(Wrappers.Group parent, Wrappers.Wrapper wrapped)
 		{
 			d_parent = parent;
 			d_wrapped = wrapped;
+			
+			d_templates = new List<Wrappers.Wrapper>();
 			
 			Wrappers.Link link = wrapped as Wrappers.Link;
 			
@@ -32,12 +37,32 @@ namespace Cpg.Studio.Undo
 			{
 				link.Attach(d_from, d_to);
 			}
+			
+			foreach (Wrappers.Wrapper templ in d_templates)
+			{
+				d_wrapped.ApplyTemplate(templ);
+			}
 
 			d_parent.Add(d_wrapped);
 		}
 		
 		protected void DoRemove()
 		{
+			Wrappers.Wrapper[] appliedto = d_wrapped.TemplateAppliesTo;
+			
+			if (appliedto.Length != 0)
+			{
+				if (appliedto.Length > 5)
+				{
+					throw new Exception(String.Format("The template is still in use by {0} objects", appliedto.Length));
+				}
+				else
+				{
+					string names = String.Join(", ", Array.ConvertAll<Wrappers.Wrapper, string>(appliedto, a => a.FullId));
+					throw new Exception(String.Format("The template is still in use by `{0}'", names));
+				}
+			}
+
 			Wrappers.Link link = d_wrapped as Wrappers.Link;
 			
 			if (link != null && link.To != null)
@@ -46,9 +71,16 @@ namespace Cpg.Studio.Undo
 				// really, and might be solved differently in the distant future, the year 2000
 				link.To.Unlink(link);
 			}
-
+			
+			d_templates = new List<Wrappers.Wrapper>(d_wrapped.AppliedTemplates);
+			
 			d_parent.Remove(d_wrapped);
 			d_wrapped.Removed();
+			
+			for (int i = d_templates.Count - 1; i >= 0; --i)
+			{
+				d_wrapped.UnapplyTemplate(d_templates[i]);
+			}
 		}
 		
 		public Wrappers.Wrapper Wrapped
